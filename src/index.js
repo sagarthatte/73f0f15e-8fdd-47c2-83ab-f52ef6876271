5c24fe1d91d4bfc3bddd7ba31fe6cf6e327a5cd7
Sagar Thatte <sagar.thatte@emersion.com>
2024-01-08 11:41:13 +0000

    Move src directory out of node_modules (correction)

---
+++ src/index.js
@@ -0,0 +1,15 @@
+// src/index.js
+
+const commander = require('commander');
+const reportGenerator = require('./src/services/reportGenerator');
+
+commander
+    .arguments('<studentId>')
+    .option('-r, --report <reportType>', 'Report Type (1: Diagnostic, 2: Progress, 3: Feedback)')
+    .action((studentId, options) => {
+        const reportType = options.report;
+        // Call and generate the request report based on parameters
+        reportGenerator.generateReport(studentId, reportType);
+    })
+    .parse(process.argv)
+;
